645809f905aa6861f199793d1ed4c61827e85f5f
Alexander Seda <alexseda04@outlook.com>
2026-01-05 19:32:26 -0600

    Organize admin add/edit forms for Seminars and Attendees

--- slts/admin.py
+++ slts/admin.py
@@ -2,11 +2,55 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Attendee, Seminar, Registration
 
-admin.site.register(Attendee)
 
 
-@admin.register(Seminar)
-class RegisterAdmin(SummernoteModelAdmin):
+class SeminarAdmin(SummernoteModelAdmin):
     summernote_fields = 'description'
 
+    fieldsets = [
+        (' ', {'fields': [
+            'status',
+            'url',
+        ]}),
 
+        ('Content', {'fields': [
+            'title',
+            'subtitle',
+            'description',
+        ]}),
+
+        ('Scheduling', {'fields': [
+            'date',
+            'time',
+            'location',
+        ]}),
+    ]
+
+
+
+class AttendeeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name / Email', {'fields': [
+            'first_name',
+            'last_name',
+            'email',
+        ]}),
+
+        ('Address', {'fields': [
+            'address',
+            'city',
+            'state',
+            'zip_code',
+        ]}),
+
+        ('Other Information', {'fields': [
+            'phone',
+            'heard_from',
+        ]}),
+    ]
+
+
+
+
+admin.site.register(Seminar, SeminarAdmin)
+admin.site.register(Attendee, AttendeeAdmin)
--- slts/models.py
+++ slts/models.py
@@ -23,7 +23,7 @@ class Seminar(models.Model):
     time = models.TimeField(default=time(10,0))
     location = models.CharField(max_length=5, choices=LOCATION_CHOICES)
     status = models.CharField(max_length=9, choices=SEMINAR_STATUS_CHOICES)
-    url = models.URLField(blank=True)
+    url = models.URLField('YouTube URL', blank=True)
 
     def __str__(self):
         return self.title
