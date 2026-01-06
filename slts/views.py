e28254519094fb3257aa39ae1d2d953914a87585
Alexander Seda <alexseda04@outlook.com>
2026-01-05 21:08:39 -0600

    Start building frontend

--- slts/views.py
+++ slts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("SLTS Homepage")
+    return render(request, "slts/home.html")
 
