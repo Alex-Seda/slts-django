7cdbf1439ac771c62d98b427959b8b11ff00b6b1
Alexander Seda <alexseda04@outlook.com>
2026-01-23 17:45:00 -0600

    Use environment variables for static and media roots
    Updated STATIC_ROOT and MEDIA_ROOT to use environment variables.

--- config/settings.py
+++ config/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = env("STATIC_ROOT")
 
 
 # Media files (User uploaded images)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = env("MEDIA_ROOT")
 
 
 # Default primary key field type
