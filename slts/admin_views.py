197e908b66494ba55b6ee2fccf8574990e3e7ca9
Alex Seda <alexseda04@outlook.com>
2026-08-17 18:53:04 -0500

    Add export all attendees button

--- slts/admin_views.py
+++ slts/admin_views.py
@@ -1,7 +1,8 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
-
-from .models import Seminar
+from .models import Seminar, Attendee
 from .services import export_sign_in_sheet_csv, export_nametags_csv, export_attendees_raw_csv
 
 @staff_member_required
@@ -20,3 +21,16 @@ def export_nametags(request, seminar_id):
 def export_raw(request, seminar_id):
     seminar = get_object_or_404(Seminar, id=seminar_id)
     return export_attendees_raw_csv(seminar)
+
+@staff_member_required
+def export_all_attendees(request):
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="attendees.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(["First Name", "Last Name", "Email", "Phone", "Address", "City", "Zip", "Birthday"])
+    for a in Attendee.objects.all().order_by("last_name", "first_name"):
+        writer.writerow([a.first_name, a.last_name, a.email, a.phone, a.address, a.city, a.zip_code, a.birthday])
+
+    return response
+
